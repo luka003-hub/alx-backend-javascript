f6901d4e2b5538513eed74754d49dadd945a6352
luka003-hub <kimathiluka03@gmail.com>
2024-07-28 14:44:13 +0000

    update

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,17 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
